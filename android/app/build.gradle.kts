12da53c1edbe2c242d2bef4189f7b8fc94800ebe
ChairulNew <chairularul010805>
2025-05-01 00:30:25 +0700

    add : koneksi ke firebase

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -2,7 +2,8 @@ plugins {
     id("com.android.application")
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
-    id("dev.flutter.flutter-gradle-plugin")
+    id("dev.flutter.flutter-gradle-plugin"),
+    id("com.google.gms.google-services") version "4.4.2" apply false
 }
 
 android {
@@ -28,6 +29,7 @@ android {
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
+        multiDexEnabled true
     }
 
     buildTypes {
