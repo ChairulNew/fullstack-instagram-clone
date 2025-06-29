db92b3a35805ca2ed99052c17e1b3fec9a16e045
ChairulNew <chairularul010805@gmail.com>
2025-06-30 06:56:31 +0700

    buat tampilan untuk post card

--- lib/widgets/post_card.dart
+++ lib/widgets/post_card.dart
@@ -72,6 +72,88 @@ class PostCard extends StatelessWidget {
               ],
             ),
           ),
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.35,
+            width: double.infinity,
+            child: Image.network(
+              "https://images.unsplash.com/photo-1555066931-bf19f8fd1085?q=80&w=1171&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+              fit: BoxFit.cover,
+            ),
+          ),
+          // like dan komen section
+          Row(
+            children: [
+              IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.favorite, color: Colors.redAccent),
+              ),
+              IconButton(onPressed: () {}, icon: Icon(Icons.comment_outlined)),
+              IconButton(onPressed: () {}, icon: Icon(Icons.send)),
+              Expanded(
+                child: Align(
+                  alignment: Alignment.bottomRight,
+                  child: IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.bookmark_border),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          // deskripsi dan komen
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                DefaultTextStyle(
+                  style: Theme.of(
+                    context,
+                  ).textTheme.bodyMedium!.copyWith(fontWeight: FontWeight.w800),
+                  child: Text(
+                    '1.211 likes',
+                    style: Theme.of(context).textTheme.bodyMedium,
+                  ),
+                ),
+                Container(
+                  width: double.infinity,
+                  padding: EdgeInsets.only(top: 8),
+                  child: RichText(
+                    text: TextSpan(
+                      style: TextStyle(color: primaryColor),
+                      children: [
+                        TextSpan(
+                          text: "username",
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        TextSpan(text: " This is description"),
+                      ],
+                    ),
+                  ),
+                ),
+
+                // comment section
+                InkWell(
+                  onTap: () {},
+                  child: Container(
+                    padding: EdgeInsets.symmetric(vertical: 4),
+                    child: Text(
+                      "lihat semua 200 komen",
+                      style: TextStyle(fontSize: 12, color: secondaryColor),
+                    ),
+                  ),
+                ),
+                Container(
+                  // padding: EdgeInsets.symmetric(vertical: 2),
+                  child: Text(
+                    "30/6/2025",
+                    style: TextStyle(fontSize: 12, color: secondaryColor),
+                  ),
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
